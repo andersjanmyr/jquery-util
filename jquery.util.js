4e49e8d553537285ecb61c71764f3993efd72f7d
Anders Janmyr <anders@janmyr.com>
2010-09-27 22:02:46 +0200

    added tapir

--- jquery.util.js
+++ jquery.util.js
@@ -1,7 +1,7 @@
 
 /* Center the selected items in the window*/
 jQuery.fn.center = function() {
-  return this.each(function(){    
+  return this.each(function(){
     function center(item) {
       return function() {
         $(item).css({left: ($(window).width() - $(item).width())/2});
@@ -10,7 +10,58 @@ jQuery.fn.center = function() {
     center(this)();
     $(window).resize(center(this));
   });
-    
+
 }
 
-  
+
+jQuery.fn.tapir = function() {
+
+  return this.each(function(){
+    var context = this.getContext('2d');
+    context.fillStyle = 'darkgray';
+    context.strokeStyle = 'darkgray';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(10, 50);
+    context.bezierCurveTo(30, 30, 30, 25, 70, 32);
+    context.bezierCurveTo(70, 32, 70, 60, 90, 60);
+    context.bezierCurveTo(83, 90, 87, 90, 87, 90);
+    context.bezierCurveTo(83, 90, 87, 90, 70, 90);
+    context.bezierCurveTo(63, 60, 67, 60, 67, 60);
+    context.bezierCurveTo(55, 57, 55, 55, 50, 50);
+    context.bezierCurveTo(12, 50, 12, 70, 12, 60);
+    context.fill();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(55, 32);
+    context.bezierCurveTo(47, 10, 59, 10, 55, 32);
+    context.bezierCurveTo(59, 10, 80, 10, 55, 32);
+    context.fill();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(125, 60);
+    context.bezierCurveTo(123, 60, 127, 60, 155, 60);
+    context.bezierCurveTo(147, 90, 147, 90, 147, 90);
+    context.bezierCurveTo(113, 90, 107, 90, 130, 90);
+    context.fill();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(125, 61);
+    context.bezierCurveTo(87, 70, 67, 50, 87, 60);
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(68, 32);
+    context.bezierCurveTo(150, 20, 150, 30, 153, 60);
+    context.stroke();
+    context.closePath();
+
+  });
+
+}
+
+
--- test/jquery.util.test.js
+++ test/jquery.util.test.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-  module("qunit.util.test");    
-    
+  module("qunit.util.test");
+
   test("center in window", function() {
     $("#center-item").center();
     ok( true, "this test is fine" );
     var value = "hello";
     equals( "hello", value, "We expect value to be hello" );
   });
+
+  test('tapir on canvas', function() {
+    $('#canvas').tapir();
+  })
 });
