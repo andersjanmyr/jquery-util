09452cf84285c6bc14bdc1d9db3c3c4ead6e1a94
Anders Janmyr <anders@janmyr.com>
2010-09-28 08:41:43 +0200

    Refactored

--- jquery.util.js
+++ jquery.util.js
@@ -16,52 +16,72 @@ jQuery.fn.center = function() {
 
 jQuery.fn.tapir = function() {
 
+  function initOptions(c) {
+    c.fillStyle = 'darkgray';
+    c.strokeStyle = 'darkgray';
+    c.lineWidth = 2;
+  }
+
+  function drawFront(c) {
+    c.beginPath();
+    c.moveTo(10, 50);
+    c.bezierCurveTo(30, 30, 30, 25, 70, 32);
+    c.bezierCurveTo(70, 32, 70, 60, 90, 60);
+    c.bezierCurveTo(83, 90, 87, 90, 87, 90);
+    c.bezierCurveTo(83, 90, 87, 90, 70, 90);
+    c.bezierCurveTo(63, 60, 67, 60, 67, 60);
+    c.bezierCurveTo(55, 57, 55, 55, 50, 50);
+    c.bezierCurveTo(12, 50, 12, 70, 12, 60);
+    c.fill();
+    c.closePath();
+  }
+
+  function drawFrontLeg(c) {
+    c.beginPath();
+    c.moveTo(55, 32);
+    c.bezierCurveTo(47, 10, 59, 10, 55, 32);
+    c.bezierCurveTo(59, 10, 80, 10, 55, 32);
+    c.fill();
+    c.closePath();
+  }
+
+  function drawBackLeg(c) {
+    c.beginPath();
+    c.moveTo(125, 60);
+    c.bezierCurveTo(123, 60, 127, 60, 155, 60);
+    c.bezierCurveTo(147, 90, 147, 90, 147, 90);
+    c.bezierCurveTo(113, 90, 107, 90, 130, 90);
+    c.fill();
+    c.closePath();
+  }
+
+  function drawStomach(c) {
+    c.beginPath();
+    c.moveTo(125, 61);
+    c.bezierCurveTo(87, 70, 67, 50, 87, 60);
+    c.stroke();
+    c.closePath();
+  }
+
+  function drawBack(c) {
+    c.beginPath();
+    c.moveTo(68, 32);
+    c.bezierCurveTo(150, 20, 150, 30, 153, 60);
+    c.stroke();
+    c.closePath();
+  }
+
   return this.each(function(){
-    var context = this.getContext('2d');
-    context.fillStyle = 'darkgray';
-    context.strokeStyle = 'darkgray';
-    context.lineWidth = 2;
-    context.beginPath();
-    context.moveTo(10, 50);
-    context.bezierCurveTo(30, 30, 30, 25, 70, 32);
-    context.bezierCurveTo(70, 32, 70, 60, 90, 60);
-    context.bezierCurveTo(83, 90, 87, 90, 87, 90);
-    context.bezierCurveTo(83, 90, 87, 90, 70, 90);
-    context.bezierCurveTo(63, 60, 67, 60, 67, 60);
-    context.bezierCurveTo(55, 57, 55, 55, 50, 50);
-    context.bezierCurveTo(12, 50, 12, 70, 12, 60);
-    context.fill();
-    context.closePath();
-
-    context.beginPath();
-    context.moveTo(55, 32);
-    context.bezierCurveTo(47, 10, 59, 10, 55, 32);
-    context.bezierCurveTo(59, 10, 80, 10, 55, 32);
-    context.fill();
-    context.closePath();
-
-    context.beginPath();
-    context.moveTo(125, 60);
-    context.bezierCurveTo(123, 60, 127, 60, 155, 60);
-    context.bezierCurveTo(147, 90, 147, 90, 147, 90);
-    context.bezierCurveTo(113, 90, 107, 90, 130, 90);
-    context.fill();
-    context.closePath();
-
-    context.beginPath();
-    context.moveTo(125, 61);
-    context.bezierCurveTo(87, 70, 67, 50, 87, 60);
-    context.stroke();
-    context.closePath();
-
-    context.beginPath();
-    context.moveTo(68, 32);
-    context.bezierCurveTo(150, 20, 150, 30, 153, 60);
-    context.stroke();
-    context.closePath();
-
+    var c = this.getContext('2d');
+    initOptions(c);
+    drawFront(c);
+    drawFrontLeg(c);
+    drawBackLeg(c);
+    drawStomach(c);
+    drawBack(c);
   });
 
 }
+
 
 
